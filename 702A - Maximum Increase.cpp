8d2d5865e2e2b8be4c9ad9f92f37f8788780c618
Abhishek Pandey <abhishekpandeyhindustani@gmail.com>
2025-01-04 23:13:27 +0530

    Added 702A - Maximum Increase

---
+++ 702A - Maximum Increase.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; 
+    cin >> n;
+    vector<int>a(n);
+    for(int i=0; i<n; i++) cin >> a[i];
+    int ans = 1, mx = 0;
+    for(int i = 1; i < n; i++){
+        if(a[i - 1] < a[i]) ans++;
+        else ans = 1;
+        mx = max(mx, ans);
+    }
+    cout << mx << endl;
+    return 0;
+}
