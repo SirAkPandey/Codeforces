a7a5fba47fedaa8e274a96e6b96064301c03bbc7
Abhishek Pandey <abhishekpandeyhindustani@gmail.com>
2025-01-21 21:27:24 +0530

    Added 1373A - Donut Shops

---
+++ 1373A - Donut Shops.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int t;
+    long long a,b,c,f,s;
+    cin>>t;
+    while(t--){
+        f = -1,s = -1;
+        cin>>a>>b>>c;
+
+        if(a < c){
+            f = 1;
+        }
+        if(a*b > c){
+            s = b;
+        }
+        cout<<f<<" "<<s<<endl;
+    }
+
+    return 0;
+}
