38064c74c25fc4306b3d99a92cf2fb8d9e1e144b
Abhishek Pandey <abhishekpandeyhindustani@gmail.com>
2025-01-29 20:56:54 +0530

    Corrected 109A - Lucky Sum of Digits

--- 109A - Lucky Sum of Digits.cpp
+++ 109A - Lucky Sum of Digits.cpp
@@ -1,53 +1,29 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <cstdio>
 
 int main(){
 
-    long long n, count=0, fours=0, sevens=0, sum=0;
-    cin>>n;
-    bool flag1=false, flag2=false;
+    long n(0); scanf("%ld", &n);
 
-    while(sum<=n){
-        sum += 4;
-        count++;
-        if(abs(sum-n)%7==0){
-            fours = count;
-            sevens = abs(sum-n)/7;
-            flag1=true;
-            break;
+    long numSevens(-1), numFours(-1), minDigits(1e6);
+
+    for(long testSevens = n / 7; testSevens >= 0; testSevens--){
+        if((n - 7 * testSevens) % 4 != 0){
+            continue;
+        }
+        long testFours = (n - 7 * testSevens) / 4;
+        numSevens = testSevens; numFours = testFours; 
+        minDigits = numSevens + numFours;
+        break;
+    }
+    if(numSevens >= 0 && numFours >= 0) {
+        for(int k = 0; k < numFours; k++){
+            printf("4");
+        }
+        for(int k = 0; k < numSevens; k++){
+            printf("7");
         }
     }
-
-    if(flag1){
-        sum=0;
-        for(int i=1; i<=fours; i++) sum = sum*10 +4;
-
-        for(int i=1; i<=sevens; i++) sum = sum*10 +7;
-
-        cout<<sum<<endl;
-        return 0;
-    }
-
-    while(sum<=n){
-        sum += 7;
-        count++;
-        if(abs(sum-n)%4==0){
-            sevens = count;
-            fours = abs(sum-n)/4;
-            flag2=true;
-        }
-    }
-
-    if(flag2){
-        sum=0;
-        for(int i=1; i<=sevens; i++) sum = sum*10 +7;
-
-        for(int i=1; i<=fours; i++) sum = sum*10 +4;
-
-        cout<<sum<<endl;
-        return 0;
-    }
-    
-    cout<<-1<<endl;
-        return 0;
+    else{printf("-1");}
+    puts("");
+    return 0;
 }
