eda2b60a35011cf98c948d092a47cc11bc25ef2f
Abhishek Pandey <abhishekpandeyhindustani@gmail.com>
2025-02-03 20:55:39 +0530

    Added 1133A - Middle of the Contest

---
+++ 1133A - Middle of the Contest.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+
+int main(){
+    long long m,n,t,i,j,k,x,y,z,l,q,r,t1,t2,t3,h,h1,m1,h2,m3,h3;
+
+    long long cnt=0,ans=0;
+    scanf("%lld:%lld",&h,&m);
+    scanf("%lld:%lld",&h1,&m1);
+
+    t1=h*60+m;
+    t2=h1*60+m1;
+
+    t3=(t1+t2)/2;
+
+    h3=(t3/60);
+    m3=(t3)%60;
+
+    printf("%02lld:%02lld\n",h3,m3);
+
+    return 0;
+}
